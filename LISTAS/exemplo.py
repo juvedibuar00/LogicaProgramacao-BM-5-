ad201b4aeffa6a3ebdc4aa3fe9556c5dc18d9adc
juvedibuar00 <batchijuve@gmail.com>
2025-08-22 06:37:01 -0300

    atualização de repositório

---
+++ LISTAS/exemplo.py
@@ -0,0 +1,7 @@
+# 1. Criação de uma lista de elementos/dados
+
+lista = [1, 2, 3, 4, 5]
+
+# Imprimindo a a lista
+'''Para imprimir a lista completa, basta usar a função print, abre e fecha parênteses. Dentro dos parênteses, você coloca o nome da lista que deseja imprimir.'''
+print(lista)
